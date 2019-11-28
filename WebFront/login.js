90da03e80a74a8cae3ee7ba5c8c94a3836917916
bianca <bianca@bianca>
2019-11-27 22:04:52 -0300

    1º commit

---
+++ WebFront/login.js
@@ -0,0 +1,39 @@
+/**
+ * User login
+ */
+var button = document.getElementById("buttonLogin").addEventListener(
+		'click',
+		function(e) {
+			e.preventDefault();
+			console.log("dentro do evento");
+			var login = document.getElementById("idLogin").value;
+			var senha = document.getElementById("passwordLogin").value;
+			var persistToken;
+			var persist = document.getElementsByName("persistToken")[0];
+			if (persist.type == "checkbox" && persist.checked == true) {
+				persistToken = true;
+			} else if (persist.type == "checkbox" && persist.checked == false) {
+				persistToken = false;
+			}
+			var xmlhttp = new XMLHttpRequest();
+			xmlhttp.onreadystatechange = function() {
+				console.log("testando o onreadyStateChange");
+				if (xmlhttp.readyState == 4) {
+					console.log("readyState == 4");
+					if (xmlhttp.status == 200) {
+						window.location.replace("./pages/logged.html");
+					} else {
+						window.location.href = "./pages/error.html";
+					}
+				}
+			}
+			xmlhttp.open("POST",
+					"http://localhost:8080/CoquetailsGateway/api/users/login");
+			xmlhttp.setRequestHeader("Content-Type", "application/json");
+			var data = {
+				login : login,
+				password : senha,
+				persistToken : persistToken
+			};
+			xmlhttp.send(JSON.stringify(data));
+		});
